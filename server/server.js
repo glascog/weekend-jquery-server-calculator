1ad07f9c1a38010bb53d560f026b5fa8054d1d23
Gabe Glasco <gabeglasco@gmail.com>
2023-07-24 09:21:08 -0500

    added calculator

--- server/server.js
+++ server/server.js
@@ -15,22 +15,30 @@ app.use(express.static('server/public'))
 
 let calculationHist = [];
 let newCalculation;
-let calculation;
+let solution;
 
-function calculate() {
-    console.log('inside calculate', newCalculation.operation);
-    let solution;
-    // if(newCalculation.operation){
-       
-    // }
-       
+function calculate(newCalculation) {
+    console.log('inside calculate', );
+    if (newCalculation.operation === '+'){
+        solution = newCalculation.firstnumber + newCalculation.secondNumber
+    } if (newCalculation.operation === '-'){
+        solution = newCalculation.firstnumber - newCalculation.secondNumber
+    } if (newCalculation.operation === '*'){
+        solution = newCalculation.firstnumber * newCalculation.secondNumber
+    } if (newCalculation.operation === '/'){
+        solution = newCalculation.firstnumber / newCalculation.secondNumber
+    }       
+    newCalculation.solution = solution
+    calculationHist.push(newCalculation)
+    console.log(newCalculation)
 }
+
 
 app.get('/solution', (req, res) => {
     console.log("Arrived at /solution")
 
    
-    res.send(calculation)
+    res.send(calculationHist)
 
 })
 
@@ -38,9 +46,10 @@ app.post('/compute', (req, res) => {
     console.log("Body for compute:", req.body);
 
     newCalculation = req.body
-    // calculationHist.push(newCalculation)
 
-    console.log("currentCalculations:", calculation)
+    calculate(newCalculation)
+
+    console.log("currentCalculations:")
     res.sendStatus(201)
 
 })
